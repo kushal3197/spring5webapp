06ef98020542d84e228d286a5c091f33e26d7b22
Kushal Kothari <kushalkothari1997@gmail.com>
2024-03-22 00:42:21 +0530

    JPA Entities 2
    Annotations needed to officially make pojo as JPA Entity

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -6,48 +6,81 @@ package guru.springframework.spring5webapp.domain;
 
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * @author Kushal
  *
  */
+@Entity
 public class Author {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
 	private String firstName;
+	private String lastName;
+	private Set<Book> books;
+	
 	/**
 	 * @return the firstName
 	 */
 	public String getFirstName() {
 		return firstName;
 	}
+	
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+
 	/**
 	 * @param firstName the firstName to set
 	 */
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+	
 	/**
 	 * @return the lastName
 	 */
 	public String getLastName() {
 		return lastName;
 	}
+	
 	/**
 	 * @param lastName the lastName to set
 	 */
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+	
 	/**
 	 * @return the books
 	 */
 	public Set<Book> getBooks() {
 		return books;
 	}
+	
 	/**
 	 * @param books the books to set
 	 */
 	public void setBooks(Set<Book> books) {
 		this.books = books;
 	}
-	private String lastName;
-	private Set<Book> books;
 }
--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -4,35 +4,102 @@
  */
 package guru.springframework.spring5webapp.domain;
 
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * @author Kushal
  *
  */
+@Entity
 public class Book {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
 	private String title;
+
+	private String isbn;
+	
+	private Set<Author> authors;
+	
 	/**
 	 * @return the title
 	 */
 	public String getTitle() {
 		return title;
 	}
+	
 	/**
 	 * @param title the title to set
 	 */
 	public void setTitle(String title) {
 		this.title = title;
 	}
+	
 	/**
 	 * @return the isbn
 	 */
 	public String getIsbn() {
 		return isbn;
 	}
+	
 	/**
 	 * @param isbn the isbn to set
 	 */
 	public void setIsbn(String isbn) {
 		this.isbn = isbn;
 	}
-	private String isbn;
+	
+	/**
+	 * @param title
+	 * @param isbn
+	 * @param authors
+	 */
+	public Book(String title, String isbn, Set<Author> authors) {
+		super();
+		this.title = title;
+		this.isbn = isbn;
+		this.authors = authors;
+	}
+	
+	/**
+	 * 
+	 */
+	public Book() {
+		super();
+	}
+	
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the authors
+	 */
+	public Set<Author> getAuthors() {
+		return authors;
+	}
+	
+	/**
+	 * @param authors the authors to set
+	 */
+	public void setAuthors(Set<Author> authors) {
+		this.authors = authors;
+	}
 }
